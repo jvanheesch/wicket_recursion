a01a912b15cb77e8e6c0376fb566840042bbeaa8
Joris van Heesch <joris.vanheesch@hpcds.com>
2017-10-28 20:12:48 +0200

    refactored UnorderedListTreePanel using PanelRepeater (html = same but with less whitespace)

--- src/main/java/com/mycompany/UnorderedListTreePanel.java
+++ src/main/java/com/mycompany/UnorderedListTreePanel.java
@@ -2,8 +2,6 @@ package com.mycompany;
 
 import com.mycompany.iface.TreeNode;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
 
@@ -14,27 +12,14 @@ public class UnorderedListTreePanel<N extends TreeNode<N>> extends GenericPanel<
         super(id, model);
     }
 
+    // N required for type inference
+    @SuppressWarnings("Convert2Diamond")
     @Override
     protected void onInitialize() {
         super.onInitialize();
 
         this.add(new Label("text", this.getModel()));
 
-        this.add(new UnorderedListTreeListView("children", this.getModel()));
-    }
-
-    private class UnorderedListTreeListView extends ListView<N> {
-        private static final long serialVersionUID = -6236048229516630926L;
-
-        UnorderedListTreeListView(String id, IModel<N> rootNodeModel) {
-            super(id, new LambdaModel<>(rootNodeModel, TreeNode::getChildren));
-        }
-
-        @Override
-        protected void populateItem(ListItem<N> item) {
-            UnorderedListTreePanel<N> childPanel = new UnorderedListTreePanel<>("childPanel", item.getModel());
-            childPanel.setRenderBodyOnly(true);
-            item.add(childPanel);
-        }
+        this.add(new PanelRepeater<N>("children", new LambdaModel<>(this.getModel(), TreeNode::getChildren), UnorderedListTreePanel::new));
     }
 }
